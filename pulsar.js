7800f787eead1e79a3ca228d26e2b6a10d152565
Rodrigo Rocha <rodrigorgs@gmail.com>
2017-01-22 11:07:56 -0300

    Create scene (game state).

--- pulsar.js
+++ pulsar.js
@@ -14,127 +14,74 @@ FRAMES = {
   GOAL: 311
 };
 
-var game = new Phaser.Game(400, 300, Phaser.AUTO, 'fubajam17', { preload: preload, create: create, update: update });
+Scene = {};
 
-function preload() {
-  game.load.tilemap('fase1', 'assets/fase1.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.spritesheet('spritesheet', 'assets/spritesheet.png', 21, 21, -1, 2, 2);
-
-
-  game.load.image('player', 'assets/player.png');
-  game.load.image('tower', 'assets/tower.png');
-  game.load.image('bullet', 'assets/bullet.png');
-
-  setas = game.input.keyboard.createCursorKeys();
-  btnSlow = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-}
-
-function createTilemap() {
-  map = game.add.tilemap('fase1');
-  map.addTilesetImage('spritesheet', 'spritesheet');
-
-  layerFundo = map.createLayer('fundo');
-  layerFrente = map.createLayer('frente');
-
-  map.setCollisionBetween(1, 899);
-  game.physics.p2.convertTilemap(map, 'frente');
-
-  map.objects.Objetos.forEach(function (obj) {
-    console.log(obj.gid);
-    obj.y -= 21;
-    if (obj.gid == FRAMES.PLAYER) { // player
-      player = new Player(game, obj.x + 10, obj.y + 10);
-      game.add.existing(player);
-    } else if (obj.gid == FRAMES.TOWER) { // tower
-      var tower;
-      tower = new Tower(game, obj.x + 10, obj.y + 10, {x: 0.0, y: 0.0});
-      tower.numBullets = 8;
-      tower.timeLastExplosion = game.time.now - tower.period / 2;
-      towerGroup.add(tower);
-    } else if (obj.gid == FRAMES.GOAL) { //goal
-      var goal = game.add.sprite(obj.x + 10, obj.y + 10, 'spritesheet');
-      goal.frame = FRAMES.GOAL - 1;
-      game.physics.p2.enable(goal);
-      goal.body.category = 'goal';
-      goal.body.kinematic = true;
-      goal.body.data.shapes[0].sensor = true;
-    }
-  });
-}
-
-function create() {
-  game.physics.startSystem(Phaser.Physics.P2JS);
-  // game.physics.p2.gravity.y = 1000;
-  game.physics.p2.restitution = 0.1;
-
-  bulletGroup = game.add.group();
-  towerGroup = game.add.group();
-
-  createTilemap();
-
-  // player = new Player(game, 200, 200);
-  // game.add.existing(player);
-
-  // tower1 = new Tower(game, 100, 150, {x: 0.0, y: 0.0});
-  // tower1.numBullets = 4;
-  // tower1.phase = Math.PI / 4;
-  // game.add.existing(tower1);
-  
-  // tower2 = new Tower(game, 300, 150, {x: 0.0, y: 0.0});
-  // tower2.numBullets = 8;
-  // tower2.phase = Math.PI / 8;
-  // game.add.existing(tower2);
-
-  // tower3 = new Tower(game, 230, 150, {x: 0.0, y: 0.0});
-  // tower3.numBullets = 8;
-  // tower3.timeLastExplosion = game.time.now - tower3.period / 2;
-  // towerGroup.add(tower3);
-
-  // tower4 = new Tower(game, 300, 150, {x: 0.0, y: 0.0});
-  // tower4.numBullets = 8;
-  // tower4.timeLastExplosion = game.time.now - tower4.period / 2;
-  // towerGroup.add(tower4);
+Scene.Game = function () {
+};
+Scene.Game.prototype = {
+  preload: function () {
+    game.load.tilemap('fase1', 'assets/fase1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('spritesheet', 'assets/spritesheet.png', 21, 21, -1, 2, 2);
 
 
-  debug = game.add.text(0, 0, '', { fill: '#ffffff', fontSize: '8pt' });
-  // this.game.input.mouse.capture = true;
-}
+    game.load.image('player', 'assets/player.png');
+    game.load.image('tower', 'assets/tower.png');
+    game.load.image('bullet', 'assets/bullet.png');
 
-function update() {
-  debug.text = 'DEBUG: ';
-  checkCollisionWithBullets();
-  checkCollisionWithTowers();
-}
+    setas = game.input.keyboard.createCursorKeys();
+    btnSlow = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  },
 
-function checkCollisionWithBullets() {
-  
+  create: function () {
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    // game.physics.p2.gravity.y = 1000;
+    game.physics.p2.restitution = 0.1;
 
-  if (player.connectedBullet != null) {
-    if (player.connectedBullet._bounds == null || !Phaser.Rectangle.intersects(player.getBounds(), player.connectedBullet.getBounds())) {
-      player.connectedBullet = null;
-    }
-  } else {
-    var activeBullet = null;
-    game.world.children.forEach(function (obj) {
-      if (obj instanceof TowerBullet) {
-        if (Phaser.Rectangle.intersects(player.getBounds(), obj.getBounds())) {
-          activeBullet = obj;
-        }
+    bulletGroup = game.add.group();
+    towerGroup = game.add.group();
+
+    this.createTilemap();
+
+    debug = game.add.text(0, 0, '', { fill: '#ffffff', fontSize: '8pt' });
+  },
+
+  update: function () {
+    debug.text = 'DEBUG: ';
+  },
+
+  createTilemap: function () {
+    map = game.add.tilemap('fase1');
+    map.addTilesetImage('spritesheet', 'spritesheet');
+
+    layerFundo = map.createLayer('fundo');
+    layerFrente = map.createLayer('frente');
+
+    map.setCollisionBetween(1, 899);
+    game.physics.p2.convertTilemap(map, 'frente');
+
+    map.objects.Objetos.forEach(function (obj) {
+      console.log(obj.gid);
+      obj.y -= 21;
+      if (obj.gid == FRAMES.PLAYER) { // player
+        player = new Player(game, obj.x + 10, obj.y + 10);
+        game.add.existing(player);
+      } else if (obj.gid == FRAMES.TOWER) { // tower
+        var tower;
+        tower = new Tower(game, obj.x + 10, obj.y + 10, {x: 0.0, y: 0.0});
+        tower.numBullets = 8;
+        tower.timeLastExplosion = game.time.now - tower.period / 2;
+        towerGroup.add(tower);
+      } else if (obj.gid == FRAMES.GOAL) { //goal
+        var goal = game.add.sprite(obj.x + 10, obj.y + 10, 'spritesheet');
+        goal.frame = FRAMES.GOAL - 1;
+        game.physics.p2.enable(goal);
+        goal.body.category = 'goal';
+        goal.body.kinematic = true;
+        goal.body.data.shapes[0].sensor = true;
       }
     });
-    this.player.connectedBullet = activeBullet;
-  }
-}
-
-function checkCollisionWithTowers() {
-  // game.world.children.forEach(function (obj) {
-  //   if (obj instanceof Tower) {
-  //     if (Phaser.Rectangle.intersects(player.getBounds(), obj.getBounds())) {
-  //       obj.destroy();
-  //     }
-  //   }
-  // });
-}
+  },
+};
 
 ///////////////
 
@@ -325,3 +272,9 @@ TowerBullet.prototype.update = function () {
   // }
 }
 
+/////////////////////////////////////////////////////////
+
+var game = new Phaser.Game(400, 300, Phaser.AUTO, '');
+// game.state.add('menu', Scene.Menu);
+game.state.add('game', Scene.Game);
+game.state.start('game');
